74ef505e060907db9fbdbc3e45b444cfd5316ff0
SimbongileKiviet <lonelekiviet@gmail.com>
2022-09-13 17:24:49 +0200

    P2 coding Layer

--- neural_network.py
+++ neural_network.py
@@ -3,10 +3,16 @@ from audioop import bias
 
 import numpy as np
 
-inputs =  [1.0, 2.0, 3.0]
-weights = [0.2, 0.8, -0.5]
-bias = 2
+inputs =  [1.0, 2.0, 3.0, 2.5]
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87] 
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
 
-out = np.multiply(inputs, weights)
-out = sum(out) + bias
+
+out = [sum(np.multiply(inputs, weights1)) + bias1, 
+    sum(np.multiply(inputs, weights2)) + bias2,
+    sum(np.multiply(inputs, weights3)) + bias3]
 print(out)
