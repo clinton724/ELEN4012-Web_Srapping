ba67324059c463b96798b36a68948ad0093b0361
clinton letsoela <clintonletsoela724c@gmail.com>
2022-09-14 00:37:13 +0200

    sacurity features

--- db.py
+++ db.py
@@ -1,9 +1,16 @@
 import pymssql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+user_name = os.getenv("userName")
+password = os.getenv("password")
+serverName = os.getenv("sqlServerName")
 
 def connectionPool ():
-            conn = pymssql.connect(server='designdb.database.windows.net', 
-                            user='designdb-admin@designdb', 
-                            password='Design2022', 
+            conn = pymssql.connect(server=serverName, 
+                            user=user_name, 
+                            password=password, 
                             database='RawData')
             return conn,conn.cursor()
 
