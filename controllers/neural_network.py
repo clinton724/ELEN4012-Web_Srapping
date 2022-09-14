d66418740b1db3bebbf123d6d0a91f0e76d0e183
SimbongileKiviet <lonelekiviet@gmail.com>
2022-09-14 17:11:58 +0200

    P4 Batches, Layers, and Objects

--- controllers/neural_network.py
+++ controllers/neural_network.py
@@ -1,9 +1,20 @@
 import numpy as np
 
-inputs =  [1.0, 2.0, 3.0, 2.5]
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-biases = [2, 3, 0.5]
-out = np.dot(weights, inputs) + biases
-print(out)
+np.random.seed(0)
+
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases 
+
+layer1 = Layer_Dense(n_inputs= 4, n_neurons=5)
+layer2 = Layer_Dense(n_inputs= 5, n_neurons=2)
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
