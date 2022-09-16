4c5a139788144acfe42e724da6a85f66fab9ad34
SimbongileKiviet <lonelekiviet@gmail.com>
2022-09-16 13:30:11 +0200

    Batch input to softmax activation

--- controllers/neural_network.py
+++ controllers/neural_network.py
@@ -1,13 +1,16 @@
+from math import exp
 import numpy as np
+import nnfs
 
-layer_outputs = [4.8, 1.21, 2.385]
+nnfs.init()
 
+layer_outputs = [[4.8, 1.21, 2.385], 
+                [8.9, -1.81, 0.2], 
+                [1.41, 1.051, 0.026]]
 
 exp_values = np.exp(layer_outputs)
-
 print(exp_values)
 
-norm_values = exp_values / np.sum(exp_values)
-
+norm_values = exp_values /np.sum(exp_values, axis=1, keepdims=True)
 print(norm_values)
-print(sum(norm_values))
+print(np.sum(norm_values, axis=1, keepdims=True))
