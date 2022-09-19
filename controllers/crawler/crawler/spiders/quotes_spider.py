c6bed4e5a4a9ee4b84bb74d9cd7fad80fac7dccd
clinton letsoela <clintonletsoela724c@gmail.com>
2022-09-19 12:38:42 +0200

    formatting scraped data

--- controllers/crawler/crawler/spiders/quotes_spider.py
+++ controllers/crawler/crawler/spiders/quotes_spider.py
@@ -51,7 +51,27 @@ class getData(scrapy.Spider):
                 yield scrapy.Request(url=index[1], callback=self.parse)
     
     def parse(self, response):
-           for index in response.css("body > div.container > div.card-body > div > div > table > tbody > tr:nth-child(n+1) > td:nth-child(n+1)::text").get():
-                     print(index)
+           name = response.css("""body > div.container > div.tw-grid.tw-grid-cols-1.lg\:tw-grid-cols-3.tw-mb-4 > 
+                    div.tw-col-span-3.md\:tw-col-span-2 > div > div.tw-col-span-2.md\:tw-col-span-2 > 
+                    div.tw-flex.tw-text-gray-900.dark\:tw-text-white.tw-mt-2.tw-items-center > div::text""").get()
+           rows = response.css("body > div.container > div.card-body > div > div > table > tbody > tr")
+           
+           for index in rows:
+                Date = index.css("th::text").extract()
+                Market_cap = index.css("td:nth-child(2)::text").extract()
+                Volume = index.css("td:nth-child(3)::text").extract()
+                Open = index.css("td:nth-child(4)::text").extract()
+                Close = index.css("td:nth-child(5)::text").extract()
+                temp0 = name.split("\n")
+                temp1 = Market_cap[0].split("\n")
+                temp2 = Volume[0].split("\n")
+                temp3 = Open[0].split("\n")
+                temp4 = Close[0].split("\n")
+                it = temp0[1]+ "/"+Date[0]+"/" +temp1[1]+"/" +temp2[1]+"/" +temp3[1]+"/" +temp4[1]
+                array = it.split("/")
+                cursor.execute(f"""""insert into HistoricalData (Cryptocurrency, Date, Market_Cap, Volume, Open_Price, Close_Price)
+                                values ({array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]})""")
+                connection.commit()
+                #print(array)
 
        
