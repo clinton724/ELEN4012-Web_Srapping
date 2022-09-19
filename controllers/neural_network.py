b31000708ec41444411ea58ab57d632d4ee05e84
SimbongileKiviet <lonelekiviet@gmail.com>
2022-09-19 09:27:15 +0200

    Combining ReLU and softmax activation

--- controllers/neural_network.py
+++ controllers/neural_network.py
@@ -1,16 +1,40 @@
-from math import exp
 import numpy as np
 import nnfs
+from nnfs.datasets import spiral_data
 
 nnfs.init()
+np.random.seed(0)
 
-layer_outputs = [[4.8, 1.21, 2.385], 
-                [8.9, -1.81, 0.2], 
-                [1.41, 1.051, 0.026]]
 
-exp_values = np.exp(layer_outputs)
-print(exp_values)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases 
 
-norm_values = exp_values /np.sum(exp_values, axis=1, keepdims=True)
-print(norm_values)
-print(np.sum(norm_values, axis=1, keepdims=True))
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0,inputs)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values /np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+X, y = spiral_data(samples=100,classes=3)
+
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
