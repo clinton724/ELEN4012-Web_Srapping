7b14b7b98dfed77644d7e14e78b607d73c1be34e
clinton letsoela <clintonletsoela724c@gmail.com>
2022-09-22 13:30:56 +0200

    front-end

--- app/views.py
+++ app/views.py
@@ -1,18 +1,26 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 
 @app.route("/")
 def home():
+    return render_template('welcome.html')
+
+@app.route("/dashboard")
+def dashboard():
     return render_template('index.html')
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template('signup.html')
+    if request.method == "POST":
+        user = request.form["name"]
+        surname = request.form["surname"]
+        email = request.form["email"]
+        password = request.form["password"]
+        print(user, " ", surname)
+        return redirect('/')
+    else:
+       return render_template('signup.html')
 
 @app.route("/login")
 def login():
     return render_template('login.html')
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
