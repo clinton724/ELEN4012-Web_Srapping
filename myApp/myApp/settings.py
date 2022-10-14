4e1a1b03124b38eb8df835b29596ea0bea91271c
clinton <clintonletsoela724c@gmail.com>
2022-10-14 20:58:27 +0200

    made changes

--- myApp/myApp/settings.py
+++ myApp/myApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l%4yf&u$7afun2%+#+^l(1w_##ak@+5p24f1oo3w(x_zyswx-m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['web-scrapingapp.azurewebsites.net']
+ALLOWED_HOSTS = ['web-scrapingapp.azurewebsites.net', '127.0.0.1']
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
